8d72314337e6cffce65439aa7205f05006b47bbb
Leo Swing <131364663+LeoSwing1@users.noreply.github.com>
2025-01-06 18:50:28 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print('Hello, world!')
+print('hey this is leo and i am starting learning python this is my first python code and will continue to learn')
+print("1")
+print('hey hey why hey hey hey hey hey hey hey hey')
+print("print run")
+print("having the same feeling as when i was learning java for the first time but that was hectic and this is smooth but thathasanother level of understanding ")
